e80b1809868e962277ecf0bf3a3af1b940b11d0d
sachithnim <nimendras07@gmail.com>
2025-05-02 16:56:16 +0530

    Add LoadingSpinner component for loading indication

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,7 @@
+const LoadingSpinner = () => {
+  return (
+    <div className="spinner-border animate-spin w-8 h-8 border-4 border-t-blue-500 rounded-full"></div>
+  );
+};
+
+export default LoadingSpinner;
