7f2c15b7754c3ccdb04756b109c0c156fb470be6
sachithnim <nimendras07@gmail.com>
2025-05-04 18:26:23 +0530

    Refactor App component for improved readability and maintainability

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
-import Home from './pages/Home';
-import CountryDetailPage from './pages/CountryDetailPage';
+"use client"
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import CountryDetailPage from "./pages/CountryDetailPage"
+import { useEffect } from "react"
 
 function App() {
+  // Add scroll to top on route change
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
   return (
     <Router>
-      <Routes>  
-        <Route path="/" element={<Home />} />  
-        <Route path="/country/:code" element={<CountryDetailPage />} /> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/country/:code" element={<CountryDetailPage />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
